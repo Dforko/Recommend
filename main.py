4433bd14f47541012ec7a1ac8386621ba76c2ffa
Dforko <118224472+Dforko@users.noreply.github.com>
2023-06-26 13:13:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -183,44 +183,6 @@ def movie_recommendation(movie_title):
     recommendations = ML_data.iloc[indices[0][1:]]['title']
 
     return recommendations
-db['description'] = db['title'] + db['overview'] + db['tagline']
-
-# Aseguramos que en los datos de las columnas title, overview y tagline no haya valores NaN y sean strings
-db['title'] = db['title'].fillna('').astype('str')
-db['overview'] = db['overview'].fillna('').astype('str')
-db['tagline'] = db['tagline'].fillna('').astype('str')
-
-cv = CountVectorizer(stop_words='english', max_features=5000)
-count_matrix = cv.fit_transform(db['description'])
-
-nn = NearestNeighbors(n_neighbors=6, metric='euclidean')
-nn.fit(count_matrix)
-
-
-smd = db.reset_index()
-titles = smd['title']
-
-indices = pd.Series(smd.index, index=smd['title'])
-
-
-@app.get('/get_recommendations/{title}')
-def get_recommendations(title:str):
-       
-    # Verificamos si el titulo ingresado se encuentra en el df
-    if title not in db['title'].values:
-        return 'La pelicula no se encuentra en el conjunto de la base de datos.'
-    else:
-        # Obtenemos el índice de la película que coincide con el título
-        index = indices[db.title]
-
-        # Obtenemos las puntuaciones de similitud de las 5 peliculas más cercanas
-        distances, indices_knn = nn.kneighbors(count_matrix[index], n_neighbors=6)  
-
-        # Obtenemos los indices de las peliculas
-        movie_indices = indices_knn[0][1:]  
-
-        # Devolvemos las 5 peliculas mas similares
-        return {'lista recomendada': db['title'].iloc[movie_indices].tolist()}
 
 #OTRO RECOMENDACIÓN 
 
